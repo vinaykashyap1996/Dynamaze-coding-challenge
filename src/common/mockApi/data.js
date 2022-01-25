79b42d848234303c36d20c4cece1b515bc6a8a9d
vinaykashyap1996 <vinay.kashyap234@gmail.com>
2022-01-25 19:59:32 +0100

    renaming of components and update mock data to render from a seperate file

---
+++ src/common/mockApi/data.js
@@ -0,0 +1,26 @@
+const mockResponse = {
+    imageUrl:
+    "https://firebasestorage.googleapis.com/v0/b/dynamaze---pwa-1550221436823.appspot.com/o/public%2Fimg%2Fdeals%2Ffunfussball1.png?alt=media&token=448599c6-ffb7-43f4-afc9-dfb82ce426f0",
+    timeSlots: [
+        {
+            id: "1",
+            ticketAmount: 10,
+            begin: "10:00",
+            price: {
+                amount: 10,
+                currency: "EUR",
+            },
+        },
+        {
+            id: "2",
+            ticketAmount: 5,
+            begin: "11:00",
+            price: {
+                amount: 12,
+                currency: "EUR",
+            },
+        },
+    ],
+};
+
+export default mockResponse;
---
+++ src/components/Hero/Hero.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <img
+            class="image"
+            :src="imageSource"
+        >
+    </div>
+</template>
+
+<script>
+import "./Hero.css";
+
+export default {
+    name: "Hero",
+    props: {
+        imageSource: {
+            type: String,
+            required: true,
+        },
+    },
+};
+</script>
